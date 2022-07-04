65ffe468134ce53e5bfb3752299e4a9496c24fd2
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-07-04 11:06:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning to main"
-  git clone https://github.com/MOH-NIHAAL/pro.git /pro
+  git clone https://github.com/MOH-NIHAAL/FileRenamerBot.git /FileRenamerBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /pro
+  git clone $UPSTREAM_REPO /FileRenamerBot
 fi
-cd /pro
+cd /FileRenamerBot
 pip3 install -U -r requirements.txt
 echo "Starting renamer....🔥"
 python3 bot.py
